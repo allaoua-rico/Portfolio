d7a88efb787600d0986fa3627e49b422a8d23b13
unknown <allaoua.boudriou@gmail.com>
2022-12-07 17:26:33 +0100

    updated links

--- components/Projects.js
+++ components/Projects.js
@@ -3,16 +3,26 @@ import React from "react";
 import styles from "../styles/Projects.module.css";
 import { projectsInfos } from "../data";
 import { FiGithub, FiExternalLink } from "react-icons/fi";
+import ImagesMuiDialog from "./shared/ImagesMuiDialog";
 
 export default function Projects() {
   return (
-    <section className="lg:px-0 px-5 flex flex-col justify-center gap-y-5 items-center pt-6 text-white w-screen lg:w-full max-w-[1000px] rounded-xl">
+    <section
+      className="lg:px-0 px-5 
+     flex flex-col justify-center gap-y-5 items-center
+     pt-6 text-white
+     w-screen lg:w-full max-w-[1000px] 
+     rounded-xl"
+    >
       <h2 className={styles.title}>Projects</h2>
       <div className="flex flex-col items-center">
         {React.Children.toArray(
           projectsInfos.map((project) => (
             <div className="flex flex-row gap-y-5 my-24 relative">
-              <ProjectImg src={project.imgs[0]} link={project.links.deployed} />
+              <ImagesMuiDialog
+                imgs={project.imgs}
+                link={project.links.deployed}
+              />
 
               <ProjectInfos>
                 <ProjectTitles project={project} />
@@ -39,7 +49,11 @@ export default function Projects() {
 
 function ProjectBottom({ children }) {
   return (
-    <div className="flex flex-col gap-y-5 opacity-95 md:opacity-100 md:bg-transparent w-full">
+    <div
+      className="w-full flex flex-col gap-y-5 
+    opacity-95 md:opacity-100 
+    md:bg-transparent py-1"
+    >
       {children}
     </div>
   );
@@ -50,22 +64,6 @@ function ProjectTitles({ project }) {
     <div className="flex flex-col md:items-end">
       <ProjectSubTitle>{project.subTitle}</ProjectSubTitle>
       <ProjectTitle>{project.title}</ProjectTitle>
-    </div>
-  );
-}
-
-function ProjectImg({ src, link }) {
-  return (
-    <div className="md:basis-2/3 w-full h-full absolute md:static">
-      <Link passHref href={link}>
-        <a className="hover:cursor-pointer relative flex group h-full">
-          <img
-            className="rounded-md z-10 object-cover w-full h-full"
-            src={src}
-          />
-          <div className="group-hover:opacity-0 transition-all duration-500 w-full h-full absolute bg-[#8B93AD] opacity-50 top-0 z-20"></div>
-        </a>
-      </Link>
     </div>
   );
 }
@@ -89,7 +87,7 @@ function ProjectInfosFctsList({ list }) {
 function ProjectInfosLink({ links }) {
   const className =
     "w-5 h-5 stroke-[#8B93AD] hover:stroke-[#85E9D9] transition-all duration-300";
-    
+
   return (
     <div className="flex w-full md:justify-end gap-x-6">
       {links.github && (
@@ -113,15 +111,27 @@ function ProjectInfosLink({ links }) {
 
 function ProjectInfosTechsList({ list }) {
   return (
-    <div className="text-[#99A2BE] flex gap-x-3 md:justify-end max-w-xs">
-      {React.Children.toArray(list?.map((el) => <div>{el}</div>))}
+    <div
+      className="text-[#99A2BE] 
+      flex space-x-3 md:justify-end 
+      md:max-w-xs"
+    >
+      {React.Children.toArray(
+        list?.map((el) => <span className="whitespace-nowrap">{el}</span>)
+      )}
     </div>
   );
 }
 
 function ProjectInfosCard({ children }) {
   return (
-    <div className="bg-[#2f4a68] opacity-95 p-6 right-0 md:w-[145%] rounded-md pl-9 z-30 w-full">
+    <div
+      className="bg-[#2f4a68] 
+    opacity-95 p-6 right-0 
+    md:w-[145%] 
+    rounded-md 
+    pl-9 z-30 w-full"
+    >
       {children}
     </div>
   );
@@ -129,7 +139,14 @@ function ProjectInfosCard({ children }) {
 
 function ProjectInfos({ children }) {
   return (
-    <div className="md:basis-1/3 basis-3/3 flex flex-col md:items-end justify-between p-8 z-30 bg-[#2f4a68] opacity-95 md:opacity-100 md:bg-transparent h-full md:h-auto">
+    <div
+      className="
+    md:basis-1/3 basis-3/3 
+    flex flex-col md:items-end justify-between 
+    p-8 z-30 bg-[#2f4a68] 
+    opacity-95 md:opacity-100 
+    md:bg-transparent h-full md:h-auto"
+    >
       {children}
     </div>
   );
@@ -140,5 +157,5 @@ function ProjectSubTitle({ children }) {
 }
 
 function ProjectTitle({ children }) {
-  return <h3 className="text-2xl">{children}</h3>;
+  return <h3 className="text-2xl pb-2">{children}</h3>;
 }
---
+++ components/shared/ImagesMuiDialog.js
@@ -0,0 +1,110 @@
+import { Dialog, DialogContent, useMediaQuery, useTheme } from "@mui/material";
+import React from "react";
+import { useState } from "react";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { IoIosArrowForward, IoIosArrowBack, IoMdClose } from "react-icons/io";
+import { EffectFade } from "swiper";
+import { useEffect } from "react";
+import { useReducer } from "react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/effect-fade";
+
+export default function ImagesMuiDialog({ imgs, link }) {
+  const [openDialog, setOpenDialog] = useState(false);
+  const theme = useTheme();
+  const md = useMediaQuery(theme.breakpoints.up("md"));
+
+  return (
+    <div className="md:basis-2/3 w-full h-full absolute md:static">
+      <ImgButton imgs={imgs} onClick={() => setOpenDialog(true)} />
+      <Dialog
+        maxWidth="md"
+        fullScreen={!md}
+        open={openDialog}
+        onClose={() => setOpenDialog(false)}
+      >
+        <DialogContent className="p-6">
+          <div className="flex justify-end pb-5">
+            <button className="" onClick={() => setOpenDialog(false)}>
+              <IoMdClose className="w-6 h-6 fill-gray-500" />
+            </button>
+          </div>
+          <DialogImgsSwiper imgs={imgs} />
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
+
+function DialogImgsSwiper({ imgs }) {
+  return (
+    <Swiper autoHeight modules={[EffectFade]} effect="fade">
+      {React.Children.toArray(
+        imgs.map((img) => (
+          <SwiperSlide>
+            <img src={img} alt={img} className="object-cover border rounded shadow-sm" />
+          </SwiperSlide>
+        ))
+      )}
+      <CustomNavigation />
+    </Swiper>
+  );
+}
+
+const CustomNavigation = () => {
+  const swiper = useSwiper();
+  const ButtonClassName = " p-1 px-2 ";
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
+
+  useEffect(() => {
+    forceUpdate();
+  }, []);
+
+  return (
+    <div
+      className="flex w-fit mx-auto
+        rounded border border-gray-300 
+        divide-x-2 divide-gray-200
+        mt-4"
+    >
+      <button
+        className={ButtonClassName + " aspect-square"}
+        onClick={() => {
+          swiper.slidePrev();
+          forceUpdate();
+        }}
+      >
+        <IoIosArrowBack />
+      </button>
+      <div className={ButtonClassName + " px-3"}>{`${
+        swiper.activeIndex + 1
+      } of ${[...swiper.slides].length}`}</div>
+      <button
+        className={ButtonClassName}
+        onClick={() => {
+          swiper.slideNext();
+          forceUpdate();
+        }}
+      >
+        <IoIosArrowForward />
+      </button>
+    </div>
+  );
+};
+
+function ImgButton({ imgs, onClick }) {
+  return (
+    <button
+      onClick={onClick}
+      className="hover:cursor-pointer relative flex group h-full"
+    >
+      <img
+        className="rounded-md z-10 object-cover w-full h-full"
+        src={imgs[0]}
+      />
+      <div className="group-hover:opacity-0 transition-all duration-500 w-full h-full absolute bg-[#8B93AD] opacity-50 top-0 z-20" />
+    </button>
+  );
+}
